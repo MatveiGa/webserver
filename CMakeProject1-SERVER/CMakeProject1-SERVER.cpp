f6aa97becd0d902137c4ef1bf1f44f8f0ed47072
MatveiGa <matvei1065@gmail.com>
2025-04-03 16:17:31 +0300

    First commit of application (WebServer)

---
+++ CMakeProject1-SERVER/CMakeProject1-SERVER.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1-SERVER.cpp: определяет точку входа для приложения.
+#include <iostream>
+#include <boost/asio.hpp>
+#include "CMakeProject1-SERVER.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMak." << endl;
+	return 0;
+}
